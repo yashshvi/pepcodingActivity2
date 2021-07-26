0dfe8a0d710318e53cc623adcfe59a36f163a151
yashshvi <yashshvigupta01@gmail.com>
2021-07-26 14:46:32 +0530

    commit

--- command/organise.js
+++ command/organise.js
@@ -1,3 +1,5 @@
+let fs=require('fs');
+var path = require('path');
 let pt="D:\\pepcodings\\pepactivity2\\listfolder";
 let types = {
     media: ["mp4", "mkv"],
@@ -7,8 +9,70 @@ let types = {
 }
 function f2(){
   
+    // srcPth = path.join(pt,fileame[i]);
+    // des  = pt/mainFolder/docume/filea
+    let folderToBeOrganize = "D:\\pepcodings\\pepactivity2\\listfolder"; 
+    let con=fs.readdirSync(folderToBeOrganize);
+    let organizedDir = path.join(folderToBeOrganize,"Organized");
+    fs.mkdirSync(organizedDir);
+    // console.log(con);
+    for(let i=0;i<con.length;i++){
+        let tempname=con[i];
+        // console.log(tempname);// printing name of file
+     var ext = path.extname(con[i]);
+        if(ext==".mp3"|| ext==".mkv"){
+            let organizeaudio=path.join(organizedDir,"video");
+            if(!fs.existsSync(organizeaudio)){
+                    fs.mkdirSync(organizeaudio);}
+                    let source=path.join(pt,tempname);  
+                    let destination= path.join(organizeaudio,tempname); // tempname bhi kar rahi thi // path of destination folder
+                    // let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
+                    console.log(destination);
+                    // let despath=path.join(destination,lastpart);
+                    fs.copyFileSync(source,destination);
+                    // console.log("file copy");
+        }else if(ext==".zip" || ext== ".7z" || ext== "rar"|| ext== "tar" || ext== "gz"|| ext== "ar"|| ext== "iso"|| 
+        ext== "xz"){
+            let archives=path.join(organizedDir,"archives");
+            if(!fs.existsSync(archives)){
+            fs.mkdirSync(archives);
+            }
+                    let source=path.join(pt,tempname);  
+                    let destination= path.join(archives); // path of destination folder
+                    let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
+                    // console.log(lastpart);
+                    
+                    let despath=path.join(destination,lastpart);
+                    console.log(source);
+                    console.log(despath);
+                    
+                    fs.copyFileSync(source,despath);
+        }else if(ext==".docx"||ext==".doc"|| ext== ".pdf" || ext== ".xlsx"|| ext== ".xls"||ext==".odt"||
+        ext== ".ods"|| ext==".odp"||ext==".odg"|| ext==".odf"|| ext==".txt"|| ext== ".ps"|| ext==".tex"){
+            let documents=path.join(organizedDir,"document");
+            if(!fs.existsSync( documents)){
+            fs.mkdirSync(documents);
+            }
+                    let source=path.join(pt,tempname);  
+                    let destination= path.join(documents); // path of destination folder
+                    let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
+                    // console.log(lastpart);
+                    let despath=path.join(destination,lastpart);
+                    fs.copyFileSync(source,despath);
+        }else{
+            let others=path.join(organizedDir,"help");
+            if(!fs.existsSync( others)){
+            fs.mkdirSync(others);}
+                    let source=path.join(pt,tempname);  
+                    let destination= path.join(others); // path of destination folder
+                    let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
+                    // console.log(lastpart);
+                    let despath=path.join(destination,lastpart);
+                    fs.copyFileSync(source,despath);
+        }
+    }
 
-    console.log("organise command executed");
+    // console.log("organise command executed");
    
     return "f2complete";
 }
--- main.js
+++ main.js
@@ -8,7 +8,6 @@ let orgobj=require('./command/organise');
 // let treeobj=require('./command/tree');
 
 let inputarr=process.argv.slice(2); // because in 0,1 position we get node ,path
-// console.log(inputarr); 
 let filename=inputarr[0];
 
  if(filename=="tree"){
@@ -19,40 +18,28 @@ let filename=inputarr[0];
      let pf=orgobj.po;
     console.log(pf);
    
-   let con=fs.readdirSync("D:\\pepcodings\\pepactivity2\\listfolder");
-   console.log(con);
+//    let con=fs.readdirSync("D:\\pepcodings\\pepactivity2\\listfolder");
+//    console.log(con);
   
-let main="mainfolder";
-console.log(main);
-let currpath=process.cwd();
-let mainfolder=path.join(currpath,main);
-console.log(mainfolder);
-fs.mkdirSync(mainfolder);
-let arr=["media","archives","documents","others"];
-for(let i=0;i<arr.length;i++){
-       let temp=mainfolder;
-   let subfolder=path.join(temp,arr[i]);
-   console.log(subfolder);
-   fs.mkdirSync(subfolder);
-   }
 
-   for(let i=0;i<con.length;i++){
-       let tempname=con[i];
-       console.log(tempname);
-    var ext = path.extname(con[i]);
-    console.log(ext);  
-    if(ext==".png"){
-        console.log("hi");
-        let source="D:\\pepcoding\\module1\\f1.txt";   // path of source file
-        let destination="D:\\pepcoding\\module1\\mydirect"; // path of destination folder
-        let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
-        console.log(lastpart);
-        let despath=path.join(destination,lastpart);
-        fs.copyFileSync(source,despath);
-        console.log("file copy");
 
-    }
+//    for(let i=0;i<con.length;i++){
+//        let tempname=con[i];
+//        console.log(tempname);// printing name of file
+//     var ext = path.extname(con[i]);
+//     console.log(ext);  
+//     // if(ext==".png"){
+//     //     console.log("hi");
+//     //     let source="D:\\pepcoding\\module1\\f1.txt";   // path of source file
+//     //     let destination="D:\\pepcoding\\module1\\mydirect"; // path of destination folder
+//     //     let lastpart=path.basename(source);  // in process of copying file in the destination folder we actuallu put //(last part of source),then join with destination and our new folder is ready
+//     //     console.log(lastpart);
+//     //     let despath=path.join(destination,lastpart);
+//     //     fs.copyFileSync(source,despath);
+//     //     console.log("file copy");
 
-   }
+//     // }
+
+//    }
 
  }
